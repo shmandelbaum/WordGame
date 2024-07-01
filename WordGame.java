29ce2625f14e3642930a82211f2a85e50a626daf
Shoshana Mandelbaum <shoshanamandelbaum@Shoshanas-Air.fios-router.home>
2024-07-01 17:35:49 -0400

    Main class

---
+++ WordGame.java
@@ -0,0 +1,11 @@
+/**
+* WordGame class represents the main class responsible for 
+launching the PuzzleGUI for the spelling bee game
+*/
+public class WordGame {
+    public static void main(String[] args) {
+        PuzzleGUI puzzleGUI = new PuzzleGUI(); // Create an instance of PuzzleGUI
+        puzzleGUI.initializeGUI();
+    }
+}
+
