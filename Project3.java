85427cbb394ab0a68ea8cd8e7073605a40603cd9
Shoshana Mandelbaum <shoshanamandelbaum@Shoshanas-Air.fios-router.home>
2024-06-30 21:04:01 -0400

    main class

---
+++ Project3.java
@@ -0,0 +1,11 @@
+/**
+* Project3 class represents the main class responsible for 
+launching the PuzzleGUI for the spelling bee game
+*/
+public class Project3 {
+    public static void main(String[] args) {
+        PuzzleGUI puzzleGUI = new PuzzleGUI(); // Create an instance of PuzzleGUI
+        puzzleGUI.initializeGUI();
+    }
+}
+
